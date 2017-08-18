fe59d7e38d784838d2e526613155866e818f2379
Brian Hedberg <bshslowfood@gmail.com>
2017-08-18 14:57:17 -0500

    moved helper back to content_helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -68,13 +68,4 @@ module ApplicationHelper
       t('links.cancel')
     end
   end
-
-  def split_tag(value, delimiter)
-    if value.is_a? Array
-      return value unless value.join.include? delimiter
-      value.map { |str| str.partition(delimiter) }.flatten.reject(&:empty?)
-    else
-      value.to_s.partition(delimiter).reject(&:empty?)
-    end
-  end
 end
