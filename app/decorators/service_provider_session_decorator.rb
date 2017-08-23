614ab9bd706aa10f03afd1711e7f89cbf9ab84a3
Brian Hedberg <bshslowfood@gmail.com>
2017-08-23 11:56:49 -0500

    rubocop

--- app/decorators/service_provider_session_decorator.rb
+++ app/decorators/service_provider_session_decorator.rb
@@ -1,5 +1,6 @@
 class ServiceProviderSessionDecorator
   include Rails.application.routes.url_helpers
+  include ActionView::Helpers
   include LocaleHelper
   include ContentHelper
 
@@ -74,25 +75,39 @@ class ServiceProviderSessionDecorator
 
   def warning_point_text(app_flow)
     warning_qualifier = I18n.t("#{app_flow}.cancel.warning_qualifier")
-    warning_qualifier_tag = "<span class='italic'>#{warning_qualifier}</span>"
+    warning_qualifier_tag = content_tag(:span, warning_qualifier, class: 'italic')
+
     if sp_name
-      sp_name_tag = "<span class='bold'>#{sp_name}</span>"
-      warning_point_text = I18n.t("#{app_flow}.cancel.warning_point",
-        sp_name: sp_name_tag, warning_qualifier: warning_qualifier_tag)
-      split_tag(
-        split_tag(warning_point_text, sp_name_tag),
-        warning_qualifier_tag
-      ).join.html_safe
+      warning_point_text_t(app_flow, warning_qualifier_tag)
     else
-      warning_point_text = I18n.t("#{app_flow}.cancel.warning_point_no_sp",
-        warning_qualifier: warning_qualifier_tag)
-      split_tag(warning_point_text, warning_qualifier_tag).join.html_safe
+      warning_point_text_t_no_sp(app_flow, warning_qualifier_tag)
     end
   end
 
   private
 
   attr_reader :sp, :view_context, :sp_session, :service_provider_request
+
+  def warning_point_text_t_no_sp(app_flow, warning_qualifier_tag)
+    warning_point_text = I18n.t(
+      "#{app_flow}.cancel.warning_point_no_sp",
+      warning_qualifier: warning_qualifier_tag
+    )
+    safe_join(split_tag(warning_point_text, warning_qualifier_tag))
+  end
+
+  def warning_point_text_t(app_flow, warning_qualifier_tag)
+    sp_name_tag = content_tag(:span, sp_name, class: 'bold')
+    warning_point_text = I18n.t(
+      "#{app_flow}.cancel.warning_point",
+      sp_name: sp_name_tag,
+      warning_qualifier: warning_qualifier_tag
+    )
+    safe_join(split_tag(
+                split_tag(warning_point_text, sp_name_tag),
+                warning_qualifier_tag
+    ))
+  end
 
   def request_url
     sp_session[:request_url] || service_provider_request.url
--- app/decorators/session_decorator.rb
+++ app/decorators/session_decorator.rb
@@ -45,19 +45,35 @@ class SessionDecorator
 
   def warning_point_text(app_flow)
     warning_qualifier = I18n.t("#{app_flow}.cancel.warning_qualifier")
-    warning_qualifier_tag = "<span class='italic'>#{warning_qualifier}</span>"
+    warning_qualifier_tag = content_tag(:span, warning_qualifier, class: 'italic')
+
     if sp_name
-      sp_name_tag = "<span class='bold'>#{sp_name}</span>"
-      warning_point_text = I18n.t("#{app_flow}.cancel.warning_point",
-        sp_name: sp_name_tag, warning_qualifier: warning_qualifier_tag)
-      split_tag(
-        split_tag(warning_point_text, sp_name_tag),
-        warning_qualifier_tag
-      ).join.html_safe
+      warning_point_text_t(app_flow, warning_qualifier_tag)
     else
-      warning_point_text = I18n.t("#{app_flow}.cancel.warning_point_no_sp",
-        warning_qualifier: warning_qualifier_tag)
-      split_tag(warning_point_text, warning_qualifier_tag).join.html_safe
+      warning_point_text_t_no_sp(app_flow, warning_qualifier_tag)
     end
+  end
+
+  private
+
+  def warning_point_text_t_no_sp(app_flow, warning_qualifier_tag)
+    warning_point_text = I18n.t(
+      "#{app_flow}.cancel.warning_point_no_sp",
+      warning_qualifier: warning_qualifier_tag
+    )
+    safe_join(split_tag(warning_point_text, warning_qualifier_tag))
+  end
+
+  def warning_point_text_t(app_flow, warning_qualifier_tag)
+    sp_name_tag = content_tag(:span, sp_name, class: 'bold')
+    warning_point_text = I18n.t(
+      "#{app_flow}.cancel.warning_point",
+      sp_name: sp_name_tag,
+      warning_qualifier: warning_qualifier_tag
+    )
+    safe_join(split_tag(
+                split_tag(warning_point_text, sp_name_tag),
+                warning_qualifier_tag
+    ))
   end
 end
